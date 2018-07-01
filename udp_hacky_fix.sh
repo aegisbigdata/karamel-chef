726efc2003816457916306f612fd99295107b5c7
Alex Ormenisan <ormenisan.adrian@gmail.com>
2018-07-01 19:01:59 +0200

    update files for dela

--- udp_hacky_fix.sh
+++ udp_hacky_fix.sh
@@ -3,7 +3,7 @@
 echo "this is a hack fix for udp port forwarding - waiting for 1min - be patient"
 echo "if nc prints a warning/error - the dela service might not work on your installation"
 sleep 1m
-echo "abc" | nc -u localhost 42011
-echo "abc" | nc -u localhost 42012
-echo "abc" | nc -u localhost 42013
+echo "abc" | nc -u localhost 58581
+echo "abc" | nc -u localhost 58582
+echo "abc" | nc -u localhost 58583
 echo "hack completed"
